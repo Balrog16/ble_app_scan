e8a52fe2ace30b58b9274041c9dd05a6871304f5
SBhamidipati <surya.b@gmail.com>
2022-02-23 10:25:25 +0530

    ble stack init and dummy hanlder

--- main.c
+++ main.c
@@ -7,7 +7,51 @@
 #include "nrf_log_default_backends.h"
 #include "app_timer.h"
 #include "nrf_pwr_mgmt.h"
+#include "ble.h"
+#include "nrf_sdh.h"
+#include "nrf_sdh_ble.h"
 
+#define APP_BLE_CONN_CFG_TAG    1                                       /**< Tag that refers to the BLE stack configuration set with @ref sd_ble_cfg_set. The default tag is @ref BLE_CONN_CFG_TAG_DEFAULT. */
+#define APP_BLE_OBSERVER_PRIO   3                                       /**< BLE observer priority of the application. There is no need to modify this value. */
+
+
+
+
+
+/**@brief Function for handling BLE events.
+ *
+ * @param[in]   p_ble_evt   Bluetooth stack event.
+ * @param[in]   p_context   Unused.
+ */
+static void ble_evt_handler(ble_evt_t const * p_ble_evt, void * p_context)
+{
+
+}
+
+/**@brief Function for initializing the BLE stack.
+ *
+ * @details Initializes the SoftDevice and the BLE event interrupt.
+ */
+static void ble_stack_init(void)
+{
+    ret_code_t err_code;
+
+    err_code = nrf_sdh_enable_request();
+    APP_ERROR_CHECK(err_code);
+
+    // Configure the BLE stack using the default settings.
+    // Fetch the start address of the application RAM.
+    uint32_t ram_start = 0;
+    err_code = nrf_sdh_ble_default_cfg_set(APP_BLE_CONN_CFG_TAG, &ram_start);
+    APP_ERROR_CHECK(err_code);
+
+    // Enable BLE stack.
+    err_code = nrf_sdh_ble_enable(&ram_start);
+    APP_ERROR_CHECK(err_code);
+
+    // Register a handler for BLE events.
+    NRF_SDH_BLE_OBSERVER(m_ble_observer, APP_BLE_OBSERVER_PRIO, ble_evt_handler, NULL);
+}
 
 /**@brief Function for initializing power management.
  */
@@ -40,4 +84,8 @@ int main(void)
     log_init();
     timer_init();
     power_management_init();
+
+
+    /** BLE stack and other related worl **/
+    ble_stack_init();
 }
