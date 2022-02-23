70210155b0eaba8e690487061f2df9912fc41979
SBhamidipati <surya.b@gmail.com>
2022-02-23 10:20:59 +0530

    timer init

--- main.c
+++ main.c
@@ -5,8 +5,15 @@
 #include "nrf_log.h"
 #include "nrf_log_ctrl.h"
 #include "nrf_log_default_backends.h"
+#include "app_timer.h"
 
 
+/**@brief Function for initializing the timer. */
+static void timer_init(void)
+{
+    ret_code_t err_code = app_timer_init();
+    APP_ERROR_CHECK(err_code);
+}
 
 /**@brief Function for initializing the nrf log module. */
 static void log_init(void)
@@ -20,4 +27,5 @@ static void log_init(void)
 int main(void)
 {
     log_init();
+    timer_init();
 }
